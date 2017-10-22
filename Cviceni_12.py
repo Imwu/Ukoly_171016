573e1e5462821c0d2b1c05abc1dc2bcd8cf86f1a
Imwu <polackova.ivca@gmail.com>
2017-10-22 20:50:19 +0200

    Lekce 4, cvičení 12

---
+++ Cviceni_12.py
@@ -0,0 +1,12 @@
+for radek in range (5):
+    if radek + 1== 1:
+        print ("prvni radek")
+    else:
+        print ("neni prvni")
+
+#ALTERNATIVNI ZAPIS - pro pocitac je i nulty radek ten prvni
+for radek in range (5):
+    if radek== 0:
+        print ("prvni radek")
+    else:
+        print ("neni prvni")
