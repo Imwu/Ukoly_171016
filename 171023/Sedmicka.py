3aebd0133b155e70b47e8ec8149e96c26807e2d0
Imwu <polackova.ivca@gmail.com>
2017-10-27 22:48:07 +0200

    Add files via upload

---
+++ 171023/Sedmicka.py
@@ -0,0 +1,39 @@
+def najdi_k ():
+    text= """Víno teď nalej nám, ať si každej, co chce zpívá,
+    vína plnej džbán, dnes do rána budeme pít.
+    Víno nalej nám, po boji jen žízeň zbývá,
+    zítra někdo z nás třeba už nebude žít.
+
+    Než přijde den, každej má času dost,
+    zbejvá jen zapít žal, klít a mít zlost,
+    že zas je nás čím dál míň, víra je tu jen host,
+    proč se máme bít druhejm pro radost.
+
+    Víno teď nalej nám, ať si každej, co chce zpívá,
+    vína plnej džbán, dnes do rána budeme pít.
+
+    Krátí se noc, v krčmě všem slábne hlas,
+    ráno zavelí a kdekdo zláme vaz,
+    už zní rozkaz, dál bít se můžem, třeba i krást,
+    ten, kdo přežije, tomu zbyde chlast.
+    už zní rozkaz, dál bít se můžem, třeba i krást,
+    ten, kdo přežije, tomu zbyde chlast.
+
+    Víno teď nalej nám, ať si každej, co chce zpívá,
+    vína plnej džbán, dnes do rána budeme pít.
+    Víno nalej nám, po boji jen žízeň zbývá,
+    zítra někdo z nás třeba už nebude žít.
+
+    Víno teď nalej nám, ať si každej, co chce zpívá,
+    vína plnej džbán, dnes do rána budeme pít.
+    Víno nalej nám, po boji jen žízeň zbývá,
+    zítra někdo z nás třeba už nebude žít.
+
+    Zpívej až do rána, není každá bitva prohraná,
+    dojde i na pána a pak si budeme žít."""
+
+    mala= text.count ("k")
+    velka= text.count ("K")
+    print (mala + velka)
+
+najdi_k ()
