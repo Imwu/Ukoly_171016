1a6784f098534787c7a9d1be54e9f43c2c3803c6
Imwu <polackova.ivca@gmail.com>
2017-11-04 21:43:33 +0100

    Upravené cvičení 11

--- 171023/Jedenact.py
+++ 171023/Jedenact.py
@@ -1,3 +1,18 @@
+def tah (pole, cislo_policka, symbol):
+    "Vrati herni pole s danym symbolem umistenym na danou pozici."
+
+    pred= pole [:cislo_policka]
+    pozice= pole [cislo_policka]
+    za= pole [cislo_policka + 1:]
+    print ("Chces zmenit ", pozice, "za ", symbol)
+
+    pozice= pozice.replace (pozice, symbol)
+    nove_pole= pred + pozice + za
+    print (pred + pozice + za)
+    return nove_pole
+
+#KONEC DEFINICE FUNKCE
+
 def tah_hrace (pole, symbol):
     "Zepta se hrace, na kterou pozici chce hrat a zmeni pole."
 
@@ -14,13 +29,7 @@ def tah_hrace (pole, symbol):
         else:
             print ("Spatne zadani")
 
-    pred= pole [:cislo_policka]
-    pozice= pole [cislo_policka]
-    za= pole [cislo_policka + 1:]
-    print ("Vymenim ", pozice, "za ", symbol)
-
-    pozice= pozice.replace (pozice, symbol)
-    print (pred + pozice + za)
+    tah (pole, cislo_policka, symbol)
 
 #KONEC DEFINICE FUNKCE
 
